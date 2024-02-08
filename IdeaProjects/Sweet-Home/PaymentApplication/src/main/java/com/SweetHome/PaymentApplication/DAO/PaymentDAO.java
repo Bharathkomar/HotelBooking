096b08c73bc2a4ff9d592fa1a19eb91075b959cc
Bharathkomar <132776108+Bharathkomar@users.noreply.github.com>
2024-02-09 00:20:46 +0530

    Delete IdeaProjects directory

--- IdeaProjects/Sweet-Home/PaymentApplication/src/main/java/com/SweetHome/PaymentApplication/DAO/PaymentDAO.java
+++
@@ -1,9 +0,0 @@
-package com.SweetHome.PaymentApplication.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-
-public interface PaymentDAO<PaymentServiceEntity,Integer> extends JpaRepository {
-
-}
